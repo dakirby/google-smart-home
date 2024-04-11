c6817e8009d0a81bdb80b8ce688d5ea6f102a39c
dakirby <39995281+dakirby@users.noreply.github.com>
2024-04-10 19:30:35 -0600

    Added default functionality for parser

--- src/main.py
+++ src/main.py
@@ -2,12 +2,17 @@ import argparse
 from scripts import debug
 
 
+def main(args):
+    pass
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
 
     parser_debug = subparsers.add_parser("debug")
     parser_debug.set_defaults(func=debug)
+
+    parser.set_defaults(func=main)
 
     args = parser.parse_args()
 
